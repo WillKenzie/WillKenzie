d1f537f54f61cb8c15559f430eb0d058b62b9579
William <46734500+WillKenzie@users.noreply.github.com>
2022-07-11 11:39:00 +0000

    Added some summarization

---
+++ repositories/work/ai/summarization/src/main.py
@@ -0,0 +1,13 @@
+from pysummarization.nlpbase.auto_abstractor import AutoAbstractor
+from pysummarization.tokenizabledoc.simple_tokenizer import SimpleTokenizer
+from pysummarization.abstractabledoc.top_n_rank_abstractor import TopNRankAbstractor
+
+auto_abstractor = AutoAbstractor()
+auto_abstractor.tokenizable_doc = SimpleTokenizer()
+auto_abstractor.delimiter_list = [".", "\n"]
+abstractable_doc = TopNRankAbstractor()
+result_dict = auto_abstractor.summarize(document, abstractable_doc)
+
+# Output result.
+for sentence in result_dict["summarize_result"]:
+    print(sentence)
